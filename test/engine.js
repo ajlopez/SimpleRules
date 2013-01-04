6cad1f3eeaf65094d8fbd1feb3ad32cb16b8d9ed
ajlopez <webmaster@ajlopez.com>
2013-01-04 19:04:22 -0300

    Engine, first tests

---
+++ test/engine.js
@@ -0,0 +1,23 @@
+
+var simplerules = require('../'),
+    assert = require('assert');
+    
+// createEngine is defined
+
+assert.ok(simplerules.createEngine);
+assert.equal(typeof simplerules.createEngine, 'function');
+
+var engine = simplerules.createEngine();
+assert.ok(engine);
+
+var obj = { };
+var item = engine.addObject(obj);
+
+assert.equal(item.isDefined('name'), false);
+
+var value = 0;
+
+item.onSetProperty('name', function () { value = 1; });
+item.setProperty('name', 'Adam');
+assert.equal(value, 1);
+assert.equal(obj.name, 'Adam');

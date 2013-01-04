179336fd61b1843fcbce75983ffca6e6ab4ab1ed
ajlopez <webmaster@ajlopez.com>
2013-01-04 06:10:36 -0300

    Lexer nextName, with test

--- test/lexer.js
+++ test/lexer.js
@@ -7,3 +7,13 @@ var simplerules = require('../'),
 assert.ok(simplerules);
 assert.equal(typeof simplerules.Lexer, 'function');
 
+var Lexer = simplerules.Lexer;
+var TokenType = simplerules.TokenType;
+
+var lexer = new simplerules.Lexer("foo");
+var token = lexer.nextToken();
+assert.ok(token);
+assert.equal(token.value, "foo");
+assert.equal(token.type, TokenType.Name);
+assert.equal(lexer.nextToken(), null);
+

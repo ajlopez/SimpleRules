d3b77ff897dfdae6d121c7d996f4178d055d2cb5
ajlopez <webmaster@ajlopez.com>
2013-01-04 19:20:18 -0300

    Engine addTask, runSync, first test

--- test/engine.js
+++ test/engine.js
@@ -13,7 +13,11 @@ assert.ok(engine);
 var obj = { };
 var item = engine.addObject(obj);
 
+// Is defined
+
 assert.equal(item.isDefined('name'), false);
+
+// On set property
 
 var value = 0;
 
@@ -21,7 +25,12 @@ item.onSetProperty('name', function () { value = 1; });
 item.setProperty('name', 'Adam');
 assert.equal(value, 1);
 assert.equal(obj.name, 'Adam');
+
+// Get property
+
 assert.equal(item.getProperty('name'), 'Adam');
+
+// On add object
 
 var value = null;
 
@@ -29,3 +38,12 @@ engine.onAddObject(function (item) { value = item.object; });
 engine.addObject(4);
 
 assert.equal(value, 4);
+
+// Add task and run sync
+
+var value = null;
+
+engine.addTask(function () { value = 1; });
+engine.runSync();
+assert.equal(value, 1);
+

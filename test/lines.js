1277334194c3dc7fedb88e63449af38ff1db728c
ajlopez <webmaster@ajlopez.com>
2015-12-20 19:48:45 -0300

    Skip blank lines

--- test/lines.js
+++ test/lines.js
@@ -13,3 +13,21 @@ exports['parse text with indent and spaces'] = function (test) {
     test.deepEqual(result, [ { indent: 2, text: "rule" } ]);
 };
 
+exports['parse text with two lines'] = function (test) {
+    var result = lines("rule\n  when");
+    
+    test.deepEqual(result, [ 
+        { indent: 0, text: "rule" },
+        { indent: 2, text: "when" }
+    ]);
+};
+
+exports['parse text with two lines and carriage return'] = function (test) {
+    var result = lines("rule\r\n  when\r\n");
+    
+    test.deepEqual(result, [ 
+        { indent: 0, text: "rule" },
+        { indent: 2, text: "when" }
+    ]);
+};
+

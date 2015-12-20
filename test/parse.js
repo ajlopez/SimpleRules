ee6d671e3db9c951f00da4d40560347a79109776
ajlopez <webmaster@ajlopez.com>
2015-12-20 20:05:44 -0300

    Parse rule

--- test/parse.js
+++ test/parse.js
@@ -4,15 +4,30 @@ var parse = require('../lib/parse');
 exports['parse line'] = function (test) {
     var result = parse("rule");
     
-    test.deepEqual(result, [{ indent: 0, text: "rule" }]);
+    test.deepEqual(result, [{ text: "rule" }]);
 };
 
 exports['parse line and indented line'] = function (test) {
     var result = parse("rule\n  when");
     
     test.deepEqual(result, [
-        { indent: 0, text: "rule", elements: [
-            { indent: 2, text: "when" }
+        { text: "rule", elements: [
+            { text: "when" }
+        ]}
+    ]);
+};
+
+exports['parse rule'] = function (test) {
+    var result = parse("rule\n  when\n    model.temperature >= 37\n  then\n    facts.fever = true");
+    
+    test.deepEqual(result, [
+        { text: "rule", elements: [
+            { text: "when", elements: [
+                { text: "model.temperature >= 37" }
+            ] },
+            { text: "then", elements: [
+                { text: "facts.fever = true" }
+            ] }
         ]}
     ]);
 };

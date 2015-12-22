cd8205cb55b3bd7b604f55eb0e12a378cdc64d09
ajlopez <webmaster@ajlopez.com>
2015-12-22 06:08:51 -0300

    Parse rule with name data

--- test/compile.js
+++ test/compile.js
@@ -22,3 +22,27 @@ exports['compile and run simple rule'] = function (test) {
     test.strictEqual(model.fever, true);
 }
 
+exports['compile rule with additional data'] = function (test) {
+    var text = [
+        "rule",
+        "  name Rule1",
+        "  when",
+        "    model.temperature >= 37",
+        "  then",
+        "    model.fever = true"
+    ].join('\n');
+    
+    var engine = simplerules.compile(text);
+    
+    test.ok(engine.rules);
+    test.equal(engine.rules.length, 1);
+    
+    test.equal(engine.rules[0].name, "Rule1");
+    
+    var model = { temperature: 38 };
+    
+    engine.run(model);
+    
+    test.strictEqual(model.fever, true);
+}
+
